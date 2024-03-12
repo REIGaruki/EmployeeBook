521fbface74123cacd6564560fcf3efbb6fa39f6
REIGaruki <gyakujo@mail.ru>
2024-03-12 19:05:22 +0300

    Normal difficulty

--- src/Main.java
+++ src/Main.java
@@ -41,7 +41,7 @@ public class Main {
         Employee[6].setFirstName("Тимур");
         Employee[6].setMiddleName("Николаевич");
         Employee[6].setSecondName("Надзоров");
-        Employee[6].setDept(2);
+        Employee[6].setDept("2");
         Employee[6].setSalary(31000);
         Employee[7] = new Employee();
         Employee[7].setFirstName("Ирина");
@@ -53,7 +53,7 @@ public class Main {
         Employee[8].setFirstName("Алена");
         Employee[8].setMiddleName("Викторовна");
         Employee[8].setSecondName("Евклидова");
-        Employee[8].setDept(4);
+        Employee[8].setDept("4");
         Employee[8].setSalary(39000);
         Employee[9] = new Employee();
         Employee[9].setFirstName("Екатерина");
@@ -62,9 +62,9 @@ public class Main {
         Employee[9].setDept(5);
         Employee[9].setSalary(32000);
         double totalSalary = 0;
-        double minimalSalary = Employee[0].getSalary();
+        double minimalSalary = (double) Integer.MAX_VALUE;
         int minSalaryEmployee = 0;
-        double maximalSalary = Employee[0].getSalary();
+        double maximalSalary = 0;
         int maxSalaryEmployee = 0;
         for (int i = 0; i < Main.Employee.counter; i++) {
             System.out.print("Номер сотрудника: " + Employee[i].getID());
@@ -93,6 +93,56 @@ public class Main {
         System.out.println("Среднее значение зарплат: " + totalSalary / Main.Employee.counter);
         for (int i = 0; i < Main.Employee.counter; i++) {
             System.out.println(Employee[i].getName());
+        }
+        int index = 7;
+        for (int i = 0; i < Main.Employee.counter; i++) {
+            System.out.println("Номер сотрудника: " + Employee[i].getID());
+            System.out.println("Зарплата проидексированная на " + index + "% равна " + Employee[i].indexSalary(index));
+        }
+        String dept = "2";
+        totalSalary = 0;
+        minimalSalary = (double) Integer.MAX_VALUE;
+        minSalaryEmployee = 0;
+        maximalSalary = 0;
+        maxSalaryEmployee = 0;
+        int deptCount = 0;
+        System.out.println("Сотрудники отдела " + dept + ":");
+        for (int i = 0; i < Main.Employee.counter; i++) {
+            if (dept.equals(Employee[i].getDept())) {
+                System.out.print("Номер сотрудника: " + Employee[i].getID());
+                System.out.print("/ ");
+                System.out.print(Employee[i].getName());
+                System.out.print("/ ");
+                System.out.print("Зарплата: " + Employee[i].getSalary() + " рублей");
+                System.out.println();
+                deptCount++;
+                totalSalary+= Employee[i].getSalary();
+                if (Employee[i].getSalary() > maximalSalary) {
+                    maximalSalary = Employee[i].getSalary();
+                    maxSalaryEmployee = i;
+                }
+                if (Employee[i].getSalary() < minimalSalary) {
+                    minimalSalary = Employee[i].getSalary();
+                    minSalaryEmployee = i;
+                }
+            }
+        }
+        System.out.println("Общая зарплата сотрудников отдела " + dept + " за месяц: " + totalSalary);
+        System.out.println(Employee[minSalaryEmployee].getName() + " - сотрудник отдела " + dept + " с наименьшей зарплатой: "
+                + Employee[minSalaryEmployee].getSalary());
+        System.out.println(Employee[maxSalaryEmployee].getName() + " - сотрудник отдела " + dept + " с наибольшей зарплатой: "
+                + Employee[maxSalaryEmployee].getSalary());
+        System.out.println("Среднее значение зарплат отдела " + dept + ": " + totalSalary / deptCount);
+        double salary = 33000;
+        for (int i = 0; i < Main.Employee.counter; i++) {
+            if (Employee[i].getSalary() >= salary) {
+                System.out.println("Зарплата сотрудника " + Employee[i].getName() + " меньше чем " + salary);
+            }
+        }
+        for (int i = 0; i < Main.Employee.counter; i++) {
+            if (Employee[i].getSalary() < salary) {
+                System.out.println("Зарплата сотрудника " + Employee[i].getName() + " равна/больше чем " + salary);
+            }
         }
     }
 
@@ -140,6 +190,10 @@ public class Main {
         }
         public void setSalary(double salary) {
             this.salary = salary;
+        }
+        public double indexSalary(int index){
+            this.salary+= this.salary * index / 100;
+            return this.salary;
         }
     }
 }
